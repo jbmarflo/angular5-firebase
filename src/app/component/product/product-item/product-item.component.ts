f9ca046cda9b52bb611f5e1a7b495e4697501e6e
Joseph Marcilla <joseph@yarkan.pe>
2018-05-21 18:22:54 -0500

    no funciona listar

--- src/app/component/product/product-item/product-item.component.ts
+++ src/app/component/product/product-item/product-item.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../../application/service/product.service';
+import { NgForm } from '@angular/forms';
+import {Product} from '../../../domain/product/entity/product';
 
 @Component({
-  selector: 'app-product-item',
-  templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+    selector: 'app-product-item',
+    templateUrl: './product-item.component.html',
+    styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent implements OnInit {
 
-  constructor() { }
+    constructor(private productService: ProductService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.productService.getProducts();
+        this.resetForm();
+    }
 
+    onSubmit(productForm: NgForm) {
+        this.productService.insertProduct(productForm.value);
+        this.resetForm(productForm);
+    }
+
+    resetForm(productForm?: NgForm) {
+      if (productForm != null) {
+        productForm.reset();
+        this.productService.selectedProduct = new Product();
+      }
+    }
 }
--- src/app/component/product/product-list/product-list.component.ts
+++ src/app/component/product/product-list/product-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../../../domain/product/entity/product';
+import {ProductService} from '../../../application/service/product.service';
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+    public productList: Product[];
 
-  constructor() { }
+    constructor(private productService: ProductService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        return this.productService.getProducts()
+            .snapshotChanges().subscribe(item => {
+            this.productList = [];
+            item.forEach(element => {
+                let x = element.payload.toJSON();
+                x['key'] = element.key;
+                this.productList.push(x as Product);
+            });
+        });
+    }
 
 }
